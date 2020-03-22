388a51d910b7e21ddc68c2b5168a4790484c7a44
malarc01 <45378877+malarc01@users.noreply.github.com>
2020-03-22 14:13:59 -0400

    mvp

--- names/names.py
+++ names/names.py
@@ -1,5 +1,60 @@
 import time
 
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, passed_in_value):
+        # if the passed in value is less than the node
+        if passed_in_value < self.value:
+            # if there is no less than node
+            if self.left == None:
+                # make a node
+                self.left = BinarySearchTree(passed_in_value)
+            # if there is a node there
+            else:
+                # be recursive
+                self.left.insert(passed_in_value)
+        # if the passed in value is greater than the node
+        else:
+            # if there is no greater than node
+            if self.right == None:
+                # make a node
+                self.right = BinarySearchTree(passed_in_value)
+            # if a node already exists
+            else:
+                # be recursive
+                self.right.insert(passed_in_value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # if current value is right, return true
+        if target == self.value:
+            return target
+        # if target is less than current value
+        elif target < self.value:
+            # if there is no left node, return false
+            if self.left == None:
+                return None
+            # if there is a left node
+            else:
+                return self.left.contains(target)
+
+        # if target is greater than current value
+        else:
+            # if there is no right node, return false
+            if self.right == None:
+                return None
+            # if there is a right node
+            else:
+                return self.right.contains(target)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,14 +68,26 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+tree = BinarySearchTree('Testing')
+
+for name in names_1:
+    tree.insert(name)
+
+for name in names_2:
+    found_name = tree.contains(name)
+    if found_name is not None:
+        duplicates.append(found_name)
+
+
+# for name_1 in names_1:
+    # for name_2 in names_2:
+        # if name_1 == name_2:
+        # duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,16 +7,41 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+    def __repr__(self):
+        return f'RingBuffer capacity: {self.capacity} current: {self.current}'
+
     def append(self, item):
-        pass
+
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        else:
+            if self.current is None:
+                self.current = self.storage.head
+
+            self.current.value = item
+
+            if self.current.next is not None:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
-
+        current = self.storage.head
+        while current is not None:
+            list_buffer_contents.append(current.value)
+            current = current.next
         return list_buffer_contents
+
+
+# buffer = RingBuffer(5)
+# print(buffer)
+# buffer.append('Human')
+# buffer.append('China')
+
+# print(buffer)
 
 # ----------------Stretch Goal-------------------
 
